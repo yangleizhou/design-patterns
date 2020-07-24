51582fa79c22e7bce0fcb61a6ee8925af47fb7e8
leizhouyang <yangleizhou@weile.com>
2020-07-24 10:20:48 +0800

    适配器模式

--- structural/09-adapter.go
+++ structural/09-adapter.go
@@ -4,6 +4,8 @@ import "fmt"
 
 //将一个类的接口转换成客户希望的另外一个接口，使得原本由于接口不兼容而不能一起工作的那些类能一起工作
 
+//golang 实现的是 对象适配器模式
+
 var _ Target = (*adapter)(nil)
 var _ Target = (*MusicPlayer)(nil)
 var _ Adaptee = (*SoundPlayer)(nil)
@@ -15,12 +17,12 @@ type Target interface {
 
 // MusicPlayer 音乐播放器
 type MusicPlayer struct {
-	src string
+	Src string
 }
 
 // PlayMusic 播放接口
 func (mp *MusicPlayer) PlayMusic() {
-	fmt.Printf("play music:%v\n", mp.src)
+	fmt.Printf("play music:%v\n", mp.Src)
 }
 
 // Adaptee 是被适配的目标接口
@@ -30,12 +32,12 @@ type Adaptee interface {
 
 // SoundPlayer 音频播放器
 type SoundPlayer struct {
-	scr string
+	Src string
 }
 
 // PlaySound 播放接口
 func (sp *SoundPlayer) PlaySound() {
-	fmt.Printf("play sound:%v\n", sp.scr)
+	fmt.Printf("play sound:%v\n", sp.Src)
 }
 
 // adapter 适配器类,继承adapter&实现Target接口
@@ -47,4 +49,11 @@ type adapter struct {
 // PlayMusic 实现Target接口
 func (ap *adapter) PlayMusic() {
 	ap.PlaySound()
+}
+
+//NewAdapter 是Adapter的工厂函数
+func NewAdapter(adaptee Adaptee) Target {
+	return &adapter{
+		Adaptee: adaptee,
+	}
 }
